0c7fa997e5644f6c295bbe484d23818e13ca1602
Yuzyzy88 <ayu.sulistiowati@protonmail.com>
2022-07-04 00:32:21 +0700

    update new path

--- app/routes/route.js
+++ app/routes/route.js
@@ -7,10 +7,10 @@ function apply(app) {
 
   app.post('/api/register', userController.create)
   app.get('/api/profile', authController.authorize, userController.read)
-  app.put('/api/profile', authController.authorize, userController.update)
+  app.patch('/api/profile', authController.authorize, userController.update)
   app.post('/api/product', productController.create)
-  app.get('/api/product', productController.read)
-  // app.put('/api/product', productController.update)
+  app.get('/api/product', productController.list)
+  app.patch('/api/product/:id', productController.update)
   return app
 }
 
